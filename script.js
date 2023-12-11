660a4cd4a0cbc6d49639a67565c10142e5990df6
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-11 16:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 function mincost(arr)
 { 
+
+	 # Convert the input array into a min-heap
+    heapq.heapify(arr)
+    
+    # Initialize the total cost
+    total_cost = 0
+    
+    # Continue until there is only one rope left
+    while len(arr) > 1:
+        # Extract the two smallest ropes from the heap
+        rope1 = heapq.heappop(arr)
+        rope2 = heapq.heappop(arr)
+        
+        # Calculate the cost of merging the two ropes
+        cost = rope1 + rope2
+        
+        # Add the cost to the total cost
+        total_cost += cost
+        
+        # Add the merged rope back to the heap
+        heapq.heappush(arr, cost)
+    
+    # The final result is the total cost
+    return total_cost
+
 //write your code here
 // return the min cost
   
